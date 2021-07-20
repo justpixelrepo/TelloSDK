6666b1bc9080c608de8b338e6ceffb0d59209e46
codebendr-product-designer <codebendr@gmail.comm>
2021-07-20 20:02:26 +0000

    added possible tests for basic flights

--- Tests/TelloSDKTests/BasicDroneControlTests.swift
+++ Tests/TelloSDKTests/BasicDroneControlTests.swift
@@ -3,6 +3,25 @@ import TelloSDK
 
 final class BasicDroneControlTests: XCTestCase {
     
+    var tello = Tello()
+    
+    func test_takeOff_wait_land() {
+        tello.send(.takeoff)
+        tello.send(.wait(seconds: 3))
+        tello.send(.land)
+    }
+    
+    func test_multiple_commands() {
+        tello.send([.takeoff, .wait(), .land])
+    }
+    
+//    func test_multiple_commands() {
+//        tello.send {
+//            takeoff
+//            wait()
+//            land
+//        }
+//    }
     
     
 }
